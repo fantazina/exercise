f33eac1cbbaf986aebc53803e2c7cf92a58ec721
fantazina <137284904+fantazina@users.noreply.github.com>
2024-02-07 19:12:33 +0900

    계산기 메커니즘 수정

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,53 +3,51 @@ import styles from './css/Calculator.module.css';
 
 
 const Calculator = () => {
+    const[newNumber, setNewNumber] = useState('')
+    const[holdNumber, setHoldNumber] = useState('')
+    const[operation, setOperation] = useState('')
 
-const[number, setNumber] = useState('');
-const[holdNumber, setHoldNumber] = useState('');
-const[operation, setOperation] = useState('');
-
-const changeNumber = (num) => { // 누르면 해당 숫자로 변경됨
-    setNumber(number + num) 
+const inputNumber = (num) => {
+    setNewNumber(newNumber + num)
 }
 
 const removeNumber = () => {
-    if(number.length === 1) { // number.length가 1이면 setNumber 초기화
-        setNumber('')
-        
-    } else { 
-        setNumber(number.slice(0, number.length - 1)) // number.length가 1이 아니면 number.length에서 하나씩 삭제
-    }
+    if(newNumber.length === 1) {
+        setNewNumber('')
+    } else {
+        setNewNumber(newNumber.slice(0, newNumber.length -1))
+    } 
 }
 
 const clickOperation = (oper) => {
-    if(number.length !== 0) {
+    if(newNumber !== '') {
         setOperation(oper)
-        setHoldNumber(number)
-        setNumber('')
+        setHoldNumber(newNumber)
+        setNewNumber('')
+
     }
-}
+}   
 
-const onCalculation = () => {
+const onCalculator = () => {
     switch(operation) {
-
         case '%' :
-            setNumber(parseFloat(holdNumber) % parseFloat(number) + '')
+            setNewNumber(parseFloat(holdNumber) % parseFloat(newNumber) + '')
             break;
-            
+
         case '/' :
-            setNumber(parseFloat(holdNumber) / parseFloat(number) + '')
+            setNewNumber(parseFloat(holdNumber) / parseFloat(newNumber) + '')
             break;
 
         case '*' :
-            setNumber(parseFloat(holdNumber) * parseFloat(number) + '')
+            setNewNumber(parseFloat(holdNumber) * parseFloat(newNumber) + '')
             break;
 
         case '-' :
-            setNumber(parseFloat(holdNumber) - parseFloat(number) + '')
+            setNewNumber(parseFloat(holdNumber) - parseFloat(newNumber) + '')
             break;
 
         case '+' :
-            setNumber(parseFloat(holdNumber) + parseFloat(number) + '')
+            setNewNumber(parseFloat(holdNumber) + parseFloat(newNumber) + '')
             break;
 
         default :
@@ -57,37 +55,44 @@ const onCalculation = () => {
     }
 }
 
+const clearAll = () => {
+    setHoldNumber('')
+    setOperation('')
+    setNewNumber('')
+
+}
     return (
         <div className={styles.cal_container}>
             <h3 style={{ textAlign : 'center' }}>지나의 계산기</h3>
                 <div className={ styles.cal_main }>
                     <div className={ styles.showbox }>
-                        <div className={ styles.show_num }>{ number.length > 0 ? number : '0' }</div>
+                        <div className={ styles.show_num }>{ newNumber.length > 0 ? newNumber : holdNumber.length > 0 ? holdNumber : '0' }</div>
                     </div>
+
                     <div className={ styles.cal_btn }>
-                        <button className={ styles.btn_x } onClick={ () => setNumber('') }>AC</button>
-                        <button className={ styles.btn_x } onClick={ () => clickOperation('%') }>%</button>
-                        <button className={ styles.btn_x } onClick={ () => clickOperation('/') }>/</button>
-                        <button className={ styles.btn_y } style={{ fontSize : '17px', textAlign : 'center' }} onClick={ removeNumber }>Back</button>
+                        <button className={ styles.btn_x } onClick={ () => clearAll() }>AC</button>
+                        <button className={ styles.btn_x } onClick={ () => clickOperation('%')}>%</button>
+                        <button className={ styles.btn_x } onClick={ () => clickOperation('/')}>/</button>
+                        <button className={ styles.btn_y } style={{ fontSize : '17px', textAlign : 'center' }} onClick={ () => removeNumber() } >Back</button>
                         
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(7) }>7</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(8) }>8</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(9) }>9</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(7) }>7</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(8) }>8</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(9) }>9</button>
                         <button className={ styles.btn_y } onClick={ () => clickOperation('*') }>x</button>
                     
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(4) }>4</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(5) }>5</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(6) }>6</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(4) }>4</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(5) }>5</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(6) }>6</button>
                         <button className={ styles.btn_y } onClick={ () => clickOperation('-') }>-</button>
                     
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(1) }>1</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(2) }>2</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(3) }>3</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(1) }>1</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(2) }>2</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber(3) }>3</button>
                         <button className={ styles.btn_y } onClick={ () => clickOperation('+') }>+</button>
                     
-                        <button className={ styles.btn_other2 } onClick={ () => changeNumber(0) }>0</button>
-                        <button className={ styles.btn_other } onClick={ () => changeNumber(".")}>.</button>
-                        <button className={ styles.btn_y } onClick={ onCalculation }>=</button>
+                        <button className={ styles.btn_other2 } onClick={ () => inputNumber(0) }>0</button>
+                        <button className={ styles.btn_other } onClick={ () => inputNumber('.') }>.</button>
+                        <button className={ styles.btn_y } onClick={ () => onCalculator() }>=</button>
                     </div>
                 </div>
         </div>
